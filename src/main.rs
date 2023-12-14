727ada2675c46e04932db0b1aac7a856b27e4b94
armichaud <email@alexandermichaud.tech>
2023-12-14 08:05:14 -0500

    Counts stones

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::{io::{BufReader, BufRead}, fs::File};
 use nalgebra::DMatrix;
 
+const STONE: char = 'O';
+const BOX: char = '#';
+
 fn get_grid(filename: &str) -> DMatrix<char> {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
@@ -14,10 +17,26 @@ fn get_grid(filename: &str) -> DMatrix<char> {
     DMatrix::from_row_slice(nrows, grid.len() / nrows, &grid)
 }
 
+fn shift_grid(grid: &mut DMatrix<char>) -> DMatrix<char> {
+    grid.to_owned()
+}
+
+fn sum_stones(grid: &DMatrix<char>) -> i32 {
+    let mut sum = 0;
+    for i in 0..grid.nrows() {
+        for j in 0..grid.ncols() {
+            if grid[(i, j)] == STONE {
+                sum += 10 - i;
+            }
+        }
+    }
+    sum as i32
+}
+
 fn solution(filename: &str) -> i32 {
-    let grid = get_grid(filename);
-    println!("Part 1: {}", grid);
-    0
+    let mut grid = get_grid(filename);
+    grid = shift_grid(&mut grid);
+    sum_stones(&grid)
 }
 
 fn main() {
