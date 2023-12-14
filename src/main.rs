28ae22228e8f28ec41c94feda9e00f9320cfa3ed
armichaud <email@alexandermichaud.tech>
2023-12-14 11:03:07 -0500

    About to test

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,32 @@ fn get_grid(filename: &str) -> DMatrix<char> {
 }
 
 fn shift_grid(grid: &mut DMatrix<char>) -> DMatrix<char> {
-    grid.to_owned()
+    let mut cloned_grid = grid.clone();
+    for j in 0..grid.ncols() {
+        let mut row_index = 0;
+        let col = grid.column(j);
+        while row_index < col.len() {
+            let indices_and_tiles = col.iter().skip(row_index).enumerate().take_while(|(_, &c)| c != BOX).collect::<Vec<(usize, &char)>>();
+            row_index = indices_and_tiles.last().unwrap_or(&(row_index, &BOX)).0 + 1;
+            let mut items = indices_and_tiles.iter().map(|(_, c)| *c).collect::<Vec<&char>>();
+            items.sort_by(|&&a, &&b| {
+                if a == STONE && b == STONE {
+                    std::cmp::Ordering::Equal
+                } else if a == STONE {
+                    std::cmp::Ordering::Less
+                } else if b == STONE {
+                    std::cmp::Ordering::Greater
+                } else {
+                    std::cmp::Ordering::Equal
+                }
+            });
+            for i in 0..items.len() {
+                cloned_grid[(i, j)] = *items[i];
+            }
+        }
+    }
+    println!("{}", cloned_grid);
+    cloned_grid.to_owned()
 }
 
 fn sum_stones(grid: &DMatrix<char>) -> i32 {
